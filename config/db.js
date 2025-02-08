debfd3effc7af66f69cd4146cf96a096bcb19e6f
AleVadher <kavasquez@uniempresarial.edu.co>
2025-02-08 17:29:35 -0500

    create project block-mongo         modified:   .gitignore         new file:   config/db.js

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+// Se crea la conexión a la base de datos mongo con mongoose
+
+const conection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Conexión exitosa a la base de datos');
+    } catch (error) {
+        console.log('Error al conectar a la base de datos: ', error);
+    }
+};
+
+export default conection;
